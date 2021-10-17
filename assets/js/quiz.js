0caf6be0334d94ecfc2eec9a2dcabcf1ac60a9fc
Sergio Lamar <sergio.lamar@gmail.com>
2021-10-17 10:44:54 -0700

    start quiz - initial

---
+++ assets/js/quiz.js
@@ -0,0 +1,10 @@
+var taskIdCounter = 0;
+
+
+
+var buttonEl = document.querySelector("#start-quiz");
+console.log(buttonEl);
+
+buttonEl.addEventListener("click", function() {
+    alert("button clicked");
+  });

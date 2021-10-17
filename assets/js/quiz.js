6b639ccead4e91f192dd2f5295979722e78b7d9e
Sergio Lamar <sergio.lamar@gmail.com>
2021-10-17 14:19:03 -0700

    reformated q&a

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -8,89 +8,81 @@ var studentPoints = 0;
 console.log(studentName, studentPoints);
 
 // create arrays for questions with 4 multiple choice answers
-    // question 1
+var 
+// question 1
 var q1 = "A memory location that holds a single letter or number?";
+// question 1 answers
+var q1 = "Char"; // correct answer
+var q1 = "Double";
+var q1 = "Int";
+var q1 = "Word";
 
-    // question 2
+// question 2
 var q2 = "Which data structure uses LIFO?";
+// question 2 answers
+var q2 = "Array";
+var q2 = "Int";
+var q2 = "Stacks"; // correct answer
+var q2 = "Queues"; 
 
-    // question 3
+// question 3
 var q3 = "Entering Comment is a useless task, it will not help in any way.";
+
+// question 3 answers
+var q3 = "True";
+var q3 = "False"; // correct answer
 
     // question 4
 var q4 = "Int holds decemal numbers.";
-
-    //question 5
-var q5 = "One loop inside the body of another loop is called?";
-
-    // question 6
-var q6 = "What is a short section of code that is written to complete a task?";
-
-    // question 7 
-var q7 = "What is a syntax error?";
-
-    // question 8 
-var q8 = "What if FIFO?";
-
-    // question 9
-var q9 = "A do loop and a while loop are the same"
-
-    // question 10
-var q10 = "What is an HTML attribute?"
-
-// question 1 answers
-var q11 = "Char"; // correct answer
-var q12 = "Double";
-var q13 = "Int";
-var q14 = "Word";
-
-// question 2 answers
-var q21 = "Array";
-var q22 = "Int";
-var q23 = "Stacks"; // correct answer
-var q24 = "Queues"; 
-
-// question 3 answers
-var q31 = "True";
-var q32 = "False"; // correct answer
-
 // question 4 answers
-var q41 = "True";
-var q42 = "False"; // correct answer
+var q4 = "True";
+var q4 = "False"; // correct answer
 
+//question 5
+var q5 = "One loop inside the body of another loop is called?";
 // question 5 answers
-var q51 = "Loop in loop";
-var q52 = "Double loops";
-var q53 = "Nested loops"; // correct answer
-var q54 = "Stacked loops";
+var q5 = "Loop in loop";
+var q5 = "Double loops";
+var q5 = "Nested loops"; // correct answer
+var q5 = "Stacked loops";
 
+// question 6
+var q6 = "What is a short section of code that is written to complete a task?";
 // question 6 answers
-var q61 = "Buffer";
-var q62 = "Function"; // correct answer
-var q63 = "Array";
-var q64 = "DOM";
+var q6 = "Buffer";
+var q6 = "Function"; // correct answer
+var q6 = "Array";
+var q6 = "DOM";
 
+// question 7 
+var q7 = "What is a syntax error?";
 // question 7 answers
-var q71 = "An error you will never find";
-var q72 = "An err you find at the end when the program gives out a wrong value due to logic error";
-var q73 = "An error due to user error";
-var q74 = "An error caused by language rules being broken";
+var q7 = "An error you will never find";
+var q7 = "An err you find at the end when the program gives out a wrong value due to logic error";
+var q7 = "An error due to user error";
+var q7 = "An error caused by language rules being broken"; // correct answer
 
+// question 8 
+var q8 = "What if FIFO?";
 // question 8 answers
-var q81 = "First in Few out";
-var q82 = "Few in Few out";
-var q83 = "First in First out"; // correct answer
-var q84 = "Few in First out"; 
+var q8 = "First in Few out";
+var q8 = "Few in Few out";
+var q8 = "First in First out"; // correct answer
+var q8 = "Few in First out"; 
 
+// question 9
+var q9 = "A do loop and a while loop are the same";
 // question 9 answers
-var q91 = "True";
-var q92 = "Fales"; // correct answer
+var q9 = "True";
+var q9 = "Fales"; // correct answer
 
+// question 10
+var q10 = "What is an HTML attribute?";
 // question 10 answers
-var q101 = "Provides additional information about an element";
-var q102 = "A true or false variable";
-var q103 = "A control flow statement";
-var q104 = "None of the above";
+var q10 = "Provides additional information about an element";
+var q10 = "A true or false variable";
+var q10 = "A control flow statement";
+var q10 = "None of the above";
 
 
 // start quiz button
